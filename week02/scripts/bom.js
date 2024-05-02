04c8b92a674a6120e0394ed34e0507aa148a0f0d
jgcoronadocueva <jgcoronadocueva@outlook.com>
2024-05-01 21:57:39 -0500

    DOM Manipulation

---
+++ week02/scripts/bom.js
@@ -0,0 +1,26 @@
+const input = document.querySelector("#favchap")
+const button = document.querySelector("button")
+const list = document.querySelector("#list")
+
+button.addEventListener('click', () => {
+    if (input.value != "") {
+        const listItem = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        listItem.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        listItem.append(deleteButton);
+        list.append(listItem);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(listItem);
+        });
+
+        input.focus();
+        input.value = "";
+    }
+    else {
+        input.focus();
+    }
+});

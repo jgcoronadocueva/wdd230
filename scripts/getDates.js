18e662888bdac39dcfeffd782be1d354a4381ad8
jgcoronadocueva <jgcoronadocueva@outlook.com>
2024-05-07 01:09:26 -0500

    Almost done

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,13 @@
 
 const today = new Date();
 const currentYear = today.getFullYear();
+const hamButton = document.querySelector("#hamButton");
+const navMenu = document.querySelector(".menuLinks");
 
 document.querySelector("#currentYear").textContent = currentYear;
 document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+hamButton.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});

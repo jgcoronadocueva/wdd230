a26bb51e2cf22df712caa49fea685a750cb7cc48
jgcoronadocueva <jgcoronadocueva@outlook.com>
2024-05-23 12:22:22 -0500

    Project Milestone: The Chamber Home Page

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -3,3 +3,12 @@ const currentYear = today.getFullYear();
 
 document.querySelector("#currentYear").textContent = currentYear;
 document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+
+const hamButton = document.querySelector("#hamButton");
+const navMenu = document.querySelector(".menuLinks");
+
+hamButton.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});

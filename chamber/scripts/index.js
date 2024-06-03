ea86c63ed3b7766aaeaf35e14b69f8b64d387b0d
jgcoronadocueva <jgcoronadocueva@outlook.com>
2024-06-03 11:36:59 -0500

    Done

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -38,4 +38,60 @@ document.addEventListener("DOMContentLoaded", function () {
     localStorage.setItem("lastVisit", Date.now());
 });
 
-/*FORM*/
+/*DIRECTORY PAGE*/
+const directoryURL = "https://jgcoronadocueva.github.io/wdd230/data/members.json";
+const directory = document.querySelector("#directory");
+
+async function getChamberMembers() {
+    const response = await fetch(directoryURL);
+    const data = await response.json();
+    displayChamberMembers(data.companies);
+}
+
+const displayChamberMembers = (company) => {
+    companies.forEach((company) => {
+        let card = document.createElement("section");
+        let logo = document.createElement("img");
+        let name = document.createElement("h2");
+        let address = document.createElement("p");
+        let website = document.createElement("a");
+        let membership = document.createElement("h4");
+
+        logo.setAttribute("src", company.logo);
+        logo.setAttribute("alt", `${business.name} logo`);
+        logo.setAttribute("loading", "lazy");
+        logo.setAttribute("width", "auto");
+        logo.setAttribute("height", "auto");
+
+        name.textContent = `${company.name}`;
+        address.textContent = `${company.address}`;
+        membership.textContent = `${company.membership} Membership`;
+        website.setAttribute("href", `${company.url}`);
+        website.textContent = business.url;
+
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(membership);
+        card.appendChild(website);
+        directory.appendChild(card);
+    });
+};
+
+getChamberMembers();
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showListView);
+
+function showListView() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}

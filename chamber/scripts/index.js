9dfcc8e1d626502575875137dc9153cf4f011e4c
jgcoronadocueva <jgcoronadocueva@outlook.com>
2024-06-03 21:40:43 -0500

    Done

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -18,17 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const discoverSidebar = document.querySelector("#discover-sidebar");
     const lastVisit = localStorage.getItem("lastVisit");
 
+    console.log(lastVisit);
+
     if (!lastVisit) {
         discoverSidebar.innerHTML = "<p>Welcome! Let us know if you have any questions.";
     }
     else {
         let currentDate = Date.now();
-        let lastVisitDate = newDate(lastVisit).getTime;
-        let timeDifference = currentDate - lastVisitDate;
+        let timeDifference = currentDate - parseInt(lastVisit);
         let daysDifference = Math.floor(timeDifference / 86400000);
 
         if (daysDifference < 1) {
-            discoverSidebar.innerHTML = "<p>Back so soon! Awesome!<p>";
+            discoverSidebar.innerHTML = "<p>Back so soon! Awesome!</p>";
         }
         else {
             discoverSidebar.innerHTML = `<p>You last visited ${daysDifference} ${(daysDifference === 1) ? "day" : "days"} ago.</p>`;
